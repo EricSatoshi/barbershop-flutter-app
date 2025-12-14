882f6387a55098aa57a0fbeb5e4a2a162b21e7d7
Eric S. Kubo <165851927+EricSatoshi@users.noreply.github.com>
2025-12-13 23:17:23 -0300

    Fix booking model field names to match ConfirmationScreen
    Updated Booking model to use 'customerName' and 'service' fields.

--- lib/models/booking_model.dart
+++ lib/models/booking_model.dart
@@ -1,22 +1,16 @@
 /// Modelo de dados para um agendamento
 class Booking {
-  final String clientName;
-  final String serviceName;
-  final DateTime date;
-  final String time;
+  final String customerName;  final String serviceName;
+  final String service;  final String time;
 
   Booking({
-    required this.clientName,
-    required this.serviceName,
-    required this.date,
-    required this.time,
+    required this.customerName,    required this.serviceName,
+    required this.service,    required this.time,
   });
 
   // Método para gerar um resumo do agendamento
   String getSummary() {
-    return 'Cliente: $clientName\n'
-        'Serviço: $serviceName\n'
-        'Data: ${date.day}/${date.month}/${date.year}\n'
-        'Horário: $time';
+    return 'Cliente: $customerName\n'        'Serviço: $serviceName\n'
+            'Serviço: $service\n'        'Horário: $time';
   }
 }
